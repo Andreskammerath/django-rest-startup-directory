8c0234ab38a3fc5be067294976c4e3b031d17158
Andrés Kammerath <akammerath@infoxel.com>
2021-09-28 11:37:53 -0300

    adding behavior to the models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,8 +3,20 @@ from organiser.models import Startup, Tag
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=31)
-    slug = models.SlugField(max_length=31)
+    title = models.CharField(max_length=63)
+    slug = models.SlugField(
+        max_length=63,
+        help_text='A label for URL config.',
+        unique_for_month='pub_date')
     text = models.TextField()
     pub_date = models.DateField()
     startups = models.ManyToManyField(Startup)
+
+    class Meta:
+        get_latest_by = 'pub_date'
+        ordering = ['-pub_date', 'title']
+        verbose_name = "blog post"
+
+    def __str__(self):
+        date_str = self.pub_date.strftime('%Y-%m-%d')
+        return f"{date_str}: {self.title}"
