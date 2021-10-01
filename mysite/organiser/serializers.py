42c66cb0b11bb8f41baea2d3aedbec8c8b9e2a45
Andrew <andreskammerath@gmail.com>
2021-10-01 16:28:11 -0300

    Organizer serializers

---
+++ mysite/organiser/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework.serializers import ModelSerializer
+from organiser.models import Tag, Startup, NewsLink
+
+class TagSerializer(ModelSerializer):
+    
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+class StartupSerializer(ModelSerializer):
+    
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Startup
+        fields = '__all__'
+
+class NewsLinkSerializer(ModelSerializer):
+
+    startup = StartupSerializer()
+
+    class Meta:
+        model = NewsLink
+        fields = '__all__'
