712ad2feb107b79caa291263207848f833ea100a
Andrew <andreskammerath@gmail.com>
2021-10-01 17:23:19 -0300

    URLs for organiser app updated

--- mysite/organiser/urls.py
+++ mysite/organiser/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from organiser.views import TagAPIDetail, TagAPIList
 
 urlpatterns = [
-    path("tags/", TagAPIList.as_view(), name='Tags_list'),
-    path("tags/<int:pk>/", TagAPIDetail.as_view(), name='Tag_detail'),    
+    path("tags/", TagAPIList.as_view(), name='api_tag_list'),
+    path("tags/<int:pk>/", TagAPIDetail.as_view(), name='api_tag_detail'),    
 ]
