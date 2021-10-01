ece9257691d9174996f302dfea171bc43a62d85c
Andrew <andreskammerath@gmail.com>
2021-10-01 17:21:39 -0300

    Registering models from organiser in the admin

--- mysite/organiser/admin.py
+++ mysite/organiser/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from organiser.models import NewsLink, Startup, Tag
 
-# Register your models here.
+admin.site.register(NewsLink)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """Configure Tag panel"""
+
+    list_display = ("pk", "name", "slug")
+
+
+@admin.register(Startup)
+class StartupAdmin(admin.ModelAdmin):
+    """Configure Startup panel"""
+
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
